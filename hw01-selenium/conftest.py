bb317e3ea54c7d231fbcfad2051ed57833cd2ec1
artnovopolsky <artemnvp@rambler.ru>
2021-03-26 04:22:45 +0300

    Tests completed, work version

--- hw01-selenium/conftest.py
+++ hw01-selenium/conftest.py
@@ -1,15 +1,12 @@
 import pytest
 from selenium import webdriver
 
-EXEC_PATH = '/home/artnovopolsky/mailru-course/chromedriver'
-URL = 'https://target.my.com/'
 
-
-@pytest.fixture()
+@pytest.fixture
 def driver():
-    browser = webdriver.Chrome(executable_path=EXEC_PATH)
-    browser.implicitly_wait(5)
-    browser.get(URL)
-    browser.maximize_window()
-    yield browser
-    browser.quit()
+    chrome_driver = webdriver.Chrome(executable_path='/home/artnovopolsky/mailru-course/chromedriver')
+    chrome_driver.implicitly_wait(5)
+    chrome_driver.get('https://target.my.com/')
+    chrome_driver.maximize_window()
+    yield chrome_driver
+    chrome_driver.quit()
--- hw01-selenium/tests/test_mytarget.py
+++ hw01-selenium/tests/test_mytarget.py
@@ -1,10 +1,11 @@
 import pytest
-from base import logged_in_page, logged_out_page, contact_info_page
+from base import logged_in_page, logged_out_page, change_profile_page
+from base import FIO_LOCATOR, PHONE_LOCATOR, CHANGE_EMAIL_LOCATOR
 
 
 @pytest.mark.UI
 def test_login(logged_in_page):
-    assert 'Создайте рекламную кампанию' in logged_in_page.page_source
+    assert logged_in_page.current_url == 'https://target.my.com/dashboard'
 
 
 @pytest.mark.UI
@@ -13,10 +14,31 @@ def test_logout(logged_out_page):
 
 
 @pytest.mark.UI
-def test_edit_profile(contact_info_page):
-    pass
+def test_edit_profile(change_profile_page):
+    fio = ['ПитоновЗмейГорыныч', 'Питонов-Змей-Горыныч', 'Питонов Змей Горыныч']
+    phone = ['273839', '+72736373839', '27-38-39']
+    email = ['gorynych45@gm.ru', 'snake@555.ua', 'snakepythonov@gov.com']
+
+    for i in range(len(fio) if len(fio) == len(phone) == len(email) else 0):
+        modified_profile_page = change_profile_page(fio[i], phone[i], email[i])
+
+        assert fio[i] == modified_profile_page.find_element_by_xpath(FIO_LOCATOR).get_attribute('value') and \
+               phone[i] == modified_profile_page.find_element_by_xpath(PHONE_LOCATOR).get_attribute('value') and \
+               email[i] == modified_profile_page.find_element_by_xpath(CHANGE_EMAIL_LOCATOR).get_attribute('value')
 
 
+@pytest.mark.parametrize('locator, url', [
+    pytest.param(
+        "//a[contains(@class, 'center-module-statistics')]", "https://target.my.com/statistics/summary",
+        id='statistics-button',
+    ),
+    pytest.param(
+        "//a[contains(@class, 'center-module-pro')]", "https://target.my.com/pro",
+        id='pro-button',
+    ),
+])
 @pytest.mark.UI
-def test_transition_to_pages(logged_in_page):
-    pass
+def test_transition_to_pages(logged_in_page, locator, url):
+    button = logged_in_page.find_element_by_xpath(locator)
+    button.click()
+    assert logged_in_page.current_url == url
